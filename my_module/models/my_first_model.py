41a455dcbc6fafeb9cc66cdda391e60c8272f818
gustavobraga1001 <braga.gustavo@outlook.com.br>
2025-02-07 15:45:49 -0300

    adicionando campo inteiro na tabela

--- my_module/models/my_first_model.py
+++ my_module/models/my_first_model.py
@@ -1,7 +1,9 @@
-from odoo import models
+from odoo import fields, models
 
 class MyFirstModel(models.Model):
     _name = "my.first.model"
     _description = "Nosso primeiro modelo criado no Odoo"
+
+    my_first_field_integer = fields.Integer(string="Número Inteiro", required=True)
 
     
