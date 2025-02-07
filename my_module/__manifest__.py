4338f4313ba276b16317c2348db7b88ae059b227
gustavobraga1001 <braga.gustavo@outlook.com.br>
2025-02-07 15:29:24 -0300

    Adicionando apenas a pasta custom_addons

---
+++ my_module/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Meu Modulo Odoo",
+    "version": "1.0",
+    "description": "My first module",
+    "summary": "Module to class",
+    "author": "Gustavo Braga"
+}
---
+++ my_module/models/my_first_model.py
@@ -0,0 +1,7 @@
+from odoo import models
+
+class MyFirstModel(models.Model):
+    _name = "my.first.model"
+    _description = "Nosso primeiro modelo criado no Odoo"
+
+    
